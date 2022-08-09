12eb18b6c0570dc8899be5a268bbbdc069239751
Carrie <echocarriet@gmail.com>
2022-08-09 17:40:55 +0800

    建立 v1.0

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,12 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
+@import '/src/assets/stylesheets/all';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+  <hr />
+  <div class="container">
+    <button class="btn btn-primary">按鈕</button>
+    <p class="fs-3 fs-md-1">
+      調整 → _variables.scss Line313 & _utilities.scss Line462
+    </p>
   </div>
 </template>
 
